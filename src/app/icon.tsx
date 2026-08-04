b4c5607257a7c39a4dfb1e934bf6613aa8e9a7f7
alysakul <alysa@kulchenko.com>
2026-08-04 11:01:55 -0700

    fixed logo and added favicon

---
+++ src/app/icon.tsx
@@ -0,0 +1,32 @@
+import { ImageResponse } from "next/og";
+
+export const size = { width: 64, height: 64 };
+export const contentType = "image/png";
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          borderRadius: 16,
+          background: "linear-gradient(135deg, #FF9500, #F4BA41)",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <svg width="40" height="40" viewBox="0 0 24 24" fill="none">
+          <path d="M12 7H17a5 5 0 010 10H12V7z" fill="white" />
+          <path
+            d="M12 7H7a5 5 0 000 10H12V7zM12 9H7a3 3 0 000 6H12V9z"
+            fill="#112845"
+            fillRule="evenodd"
+          />
+        </svg>
+      </div>
+    ),
+    { ...size }
+  );
+}
--- src/components/BrandMark.tsx
+++ src/components/BrandMark.tsx
@@ -4,8 +4,9 @@ export default function BrandMark() {
       <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M12 7H17a5 5 0 010 10H12V7z" fill="white" />
         <path
-          d="M10.7 7.4 A5 5 0 1 0 10.7 16.6 L7.9 13.8 A1.6 1.6 0 1 1 7.9 10.2 Z"
+          d="M12 7H7a5 5 0 000 10H12V7zM12 9H7a3 3 0 000 6H12V9z"
           fill="var(--navy)"
+          fillRule="evenodd"
         />
       </svg>
     </span>
